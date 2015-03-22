1e5f883985dd1ef359d21591ae7706c0ece5cd43
Nishikar Sapkota <nishikar@Nishikars-MacBook-Air.local>
2015-03-22 17:45:54 -0500

    add test case for get note.

--- tab_converter_site/converter_core/tests.py
+++ tab_converter_site/converter_core/tests.py
@@ -7,3 +7,5 @@ class ConverterTest(unittest.TestCase):
     def test_get_note(self):
         note = get_note('C', 5)
         self.assertEqual(note, 'F')
+        note = get_note('c', 5)
+        self.assertEqual(note, 'f')
