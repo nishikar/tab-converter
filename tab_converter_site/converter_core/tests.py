9d5c4693926e96b55b7dc86608852d28da089fd0
Nishikar Sapkota <nishikar@Nishikars-MacBook-Air.local>
2015-03-22 17:42:08 -0500

    add test file and travis yaml for build

---
+++ tab_converter_site/converter_core/tests.py
@@ -0,0 +1,9 @@
+import unittest
+from converter import get_note
+
+
+class ConverterTest(unittest.TestCase):
+
+    def test_get_note(self):
+        note = get_note('C', 5)
+        self.assertEqual(note, 'F')
